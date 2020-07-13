5f84c89f8365aa41ae4a9e657b4dd72afb7129dc
Arthur Balduini <arthurbalduini@gmail.com>
2020-07-12 22:56:11 -0300

    improves form and adds util function

--- src/components/my-form/index.js
+++ src/components/my-form/index.js
@@ -1,20 +1,24 @@
 import './index.scss';
 
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
 
 import Button from '../base/Button';
-import Input from '../base/Input'
-import { emailContext } from '../../context/email'
-import { useForm } from 'react-hook-form'
+import ErrorMessage from '../base/ErrorMessage';
+import Input from '../base/Input';
+import { postEmailData } from '../../service';
+import { useForm } from 'react-hook-form';
 
 const MyForm = () => {
 
-    const test = useContext(emailContext)
-
-    const {register, handleSubmit, errors} = useForm();
-
+    const { register, handleSubmit, errors } = useForm({
+        mode: 'onSubmit',
+        reValidateMode: 'onChange',
+        defaultValues: {},
+        criteriaMode: "firstErrorDetected",
+    });
+    
     const onSubmit = data => {
-        console.log(test);
+        postEmailData(data);
     }
 
     return(
@@ -27,10 +31,13 @@ const MyForm = () => {
                     type="text" 
                     placeholder="Email" 
                     name="email" 
-                    register={register} 
+                    register={register({ required: "This is required" })} 
                     label="Email" 
                     dataTestID={'email-input'} 
                 />
+                
+                {errors.email && <ErrorMessage msg='Este campo é obrigatório' />}
+                
                 <Input type="password" placeholder="Password" name="password" register={register} label="Password" />
                 <Button type="submit" label="Submit" />
             </form>
