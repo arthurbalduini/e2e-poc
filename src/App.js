3eed3db32507b4e1b2998139c186b59f0a104609
Arthur Balduini <arthurbalduini@gmail.com>
2020-07-12 22:54:59 -0300

    removes test from context provider

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React from 'react';
 
 function App() {
   return (
-    <EmailContextProvider value="teste">
+    <EmailContextProvider >
       <div className="App">
         <MyForm />
         <LabelChanger />
